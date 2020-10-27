88b386332329a9943f2042a69d64b298111cd55b
ashoka <aashok@encoress.com>
2020-10-27 12:23:23 -0500

    Python file name renamed

--- BaseFile.py
+++ BaseFile.py
@@ -4,6 +4,12 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
 
+
+class Basefile()
+    def test(self):
+        global driver
+
+
 driver = webdriver.Chrome('P://IT Transformation -Automation//WPR//WPR//ChromeDriver//chromedriver.exe')
 driver.implicitly_wait(3)
 driver.maximize_window()
@@ -14,5 +20,4 @@ time.sleep(15)
 driver.find_element_by_xpath("//img[@src='../../../assets/images/logo.png']").is_displayed()
 # wait = WebDriverWait(driver, 15)
 # menu_menu = wait.until(EC.presence_of_element_located(By.XPATH, "//img[@src='../../../assets/images/logo.png']"))
-print("Displays")
 print("Displays")
